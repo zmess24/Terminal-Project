41314563254617062d8f6dda02209d1395c96918
Zachary Messinger <zacharymessinger@zdmessinger-macbook.local>
2021-10-02 14:58:57 -1000

    added backspace key

--- main.js
+++ main.js
@@ -4,26 +4,37 @@ console.log("JS Loaded");
  * Helper Functions + Global Variables
  */
 
-let currentString = '[zacharymessinger] ';
+let 
+    prompt = '[zacharymessinger]';
+    currentString = '';
+
+
 const commandMap = { 
     ls: 'Found LS' 
 };
 
 function createNewLine() {
     let li = document.createElement('li');
-    currentString = '[zacharymessinger] ';
-    li.innerText = currentString;
+    let line = `${prompt} ${currentString}`;
+    li.innerText = line;
     document.querySelector('ul').appendChild(li);
 };
 
 function appendCurrentLine(value) {
     let currentLine = document.querySelector('li:last-child');
     currentString = currentString + value;
-    currentLine.innerText = currentString;
+    currentLine.innerText = `${prompt} ${currentString}`;
 };
 
+function backSpace() {
+    let currentLine = document.querySelector('li:last-child');
+    currentString = currentString.slice(0, currentString.length-1);
+    currentLine.innerText = `${prompt} ${currentString}`;
+}
+
 document.addEventListener('keydown', ({ key, code }) => {
-    if (code === "Enter") { createNewLine() };
+    if (code === "Enter") createNewLine();
     if (code === 'Space') appendCurrentLine(' ');
+    if (code === "Backspace") backSpace();
     if (code.indexOf('Key') > -1) appendCurrentLine(key);
 });
